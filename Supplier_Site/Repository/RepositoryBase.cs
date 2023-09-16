ca0b7242cd546d678a67455c9b85603cf84a0d44
Szarath Kumar <szarath@users.noreply.github.com>
2023-09-16 15:37:13 +0200

    Added RepositoryBase

---
+++ Supplier_Site/Repository/RepositoryBase.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Supplier_Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Supplier_Site.Repository
+{
+    public class RepositoryBase<TEntity> where TEntity : class
+    {
+        protected readonly SupplierDbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public RepositoryBase(SupplierDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public virtual TEntity GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public virtual void Add(TEntity entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public virtual void Update(TEntity entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(int id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public virtual IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}
--- Supplier_Site/Repository/SupplierRepository.cs
+++ Supplier_Site/Repository/SupplierRepository.cs
@@ -1,43 +1,12 @@
 ﻿using Supplier_Site.Models;
+using Supplier_Site.Repository;
 
 namespace Supplier_Site.Repository
 {
-    public class SupplierRepository : ISupplierRepository
+    public class SupplierRepository : RepositoryBase<Supplier>, ISupplierRepository
     {
-        private readonly SupplierDbContext _context;
-
-        public SupplierRepository(SupplierDbContext context)
+        public SupplierRepository(SupplierDbContext context) : base(context)
         {
-            _context = context;
-        }
-
-        public IEnumerable<Supplier> GetAll()
-        {
-            return _context.Suppliers.ToList();
-        }
-
-        public Supplier GetById(int id)
-        {
-            return _context.Suppliers.FirstOrDefault(s => s.Code == id);
-        }
-
-        public void Add(Supplier supplier)
-        {
-            _context.Suppliers.Add(supplier);
-            _context.SaveChanges();
-        }
-
-        public void Update(Supplier supplier)
-        {
-            _context.Suppliers.Update(supplier);
-            _context.SaveChanges();
-        }
-
-        public void Delete(int id)
-        {
-            var supplier = _context.Suppliers.FirstOrDefault(s => s.Code == id);
-            _context.Suppliers.Remove(supplier);
-            _context.SaveChanges();
         }
     }
 }
